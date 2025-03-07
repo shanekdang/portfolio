c8c69904272c0fb13a0983a1b8957469cae2e7a4
shane <114774143+shanekdang@users.noreply.github.com>
2025-03-06 16:30:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,33 +16,25 @@ const texts = [
     "SOFTWARE ENGINEER"
 ]
 
-let speed = 100;
-let eraseSpeed = 50;
-let delayBetweenTexts = 500;
-let delayAfterText = 1000;
+let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
 
 let textIndex = 0;
 let charcterIndex = 0;
 
-function typeWriter() {
-    if (charcterIndex < texts[textIndex].length) {
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
-        setTimeout(typeWriter, speed);
-    } else {
-        setTimeout(eraseText, delayAfterText);
+        setTimeout(typeWriter, speed); 
+    }
+    else{
+        setTimeout(eraseText, 1000)
     }
 }
 
-function eraseText() {
-    if (textElements.innerHTML.length > 0) {
-        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
-        setTimeout(eraseText, eraseSpeed);
-    } else {
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, delayBetweenTexts);
-    
-
-window.onload = typeWriter;
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
