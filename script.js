347149817cc6ae42aeb9dad79ef0e31cc6c6e7c5
shane <114774143+shanekdang@users.noreply.github.com>
2025-03-06 16:27:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = (navbar.style.transform === "translateY(0px)") ? "translateY(-500px)" : "translateY(0px)";
 }
 
 function cancel(){
@@ -16,33 +16,33 @@ const texts = [
     "SOFTWARE ENGINEER"
 ]
 
-let speed  =100;
+let speed = 100;
+let eraseSpeed = 50;
+let delayBetweenTexts = 500;
+let delayAfterText = 1000;
 const textElements = document.querySelector(".typewriter-text");
 
 let textIndex = 0;
 let charcterIndex = 0;
 
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
+function typeWriter() {
+    if (charcterIndex < texts[textIndex].length) {
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, delayAfterText);
     }
 }
 
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, eraseSpeed);
+    } else {
         textIndex = (textIndex + 1) % texts.length;
         charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
+        setTimeout(typeWriter, delayBetweenTexts);
+    
 
-window.onload = typeWriter
+window.onload = typeWriter;
